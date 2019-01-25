2f04dc73870484d3401ca8b04e8ed3c2c831558b
DarkiDev <rageskilln@gmail.com>
2019-01-26 00:26:14 +0100

    [#] Erstellen der Basis-Projekte

---
+++ LegionOfCards/LegionOfCards.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LegionOfCards.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
